c243e2517468823c475b8a632e89f2ee799971a2
chikkuduchandu <160953857+chikkuduchandu@users.noreply.github.com>
2025-10-25 21:50:23 +0530

    Update palindromeLinkedlist.py

--- Day15_Palindrome_linkedList/palindromeLinkedlist.py
+++ Day15_Palindrome_linkedList/palindromeLinkedlist.py
@@ -1,1 +1,46 @@
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def findMid(self,head):
+        if head is None:
+            return head
+        s=head
+        f=head
+        while f.next is not None and f.next.next is not None:
+            f=f.next.next
+            s=s.next
+        return s.next
+
+    def reverseList(self,head):
+        if head is None or head.next is None:
+            return head
+        new_head=self.reverseList(head.next)
+
+        head.next.next=head
+        head.next=None
+        return new_head
+
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        
+        mid=self.findMid(head)
+        
+        new_head=self.reverseList(mid)
+        
+        while new_head is not None:
+            if head.val!=new_head.val:
+                return False
+            head=head.next
+            new_head=new_head.next
+        return True
+
+
+
+        
